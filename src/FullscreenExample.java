af5e823e4f224e2da278ff346fb5d562d32ccf25
Danny Van Stemp <danwardvs@adsgames.net>
2016-02-17 08:33:44 -0500

    3D cube

--- src/FullscreenExample.java
+++ src/FullscreenExample.java
@@ -12,6 +12,8 @@ public class FullscreenExample {
     float x = 400, y = 300;
     /** angle of quad rotation */
     float rotation = 0;
+    
+    float angle= 0;
   
     /** time at last frame */
     long lastFrame;
@@ -80,7 +82,8 @@ public class FullscreenExample {
   
         updateFPS(); // update FPS Counter
     }
-  
+    
+ 
     /**
      * Set the display mode to be used 
      * 
@@ -178,32 +181,67 @@ public class FullscreenExample {
     }
   
     public void initGL() {
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, 800, 0, 600, 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    	 GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+         // Clear the screen and depth buffer
+    	 GL11.glMatrixMode(GL11.GL_PROJECTION);
+    	 GL11.glLoadIdentity();
+    	 GL11.glOrtho(0, 800, 600, 0, 800, -600);
+    	 GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
     }
   
     public void renderGL() {
         // Clear The Screen And The Depth Buffer
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-  
-        // R,G,B,A Set The Color To Blue One Time Only
-        GL11.glColor3f(0.5f, 0.5f, 1.0f);
-  
-        // draw quad
-        GL11.glPushMatrix();
-            GL11.glTranslatef(x, y, 0);
-            GL11.glRotatef(rotation, 0f, 0f, 1f);
-            GL11.glTranslatef(-x, -y, 0);
-  
-            GL11.glBegin(GL11.GL_QUADS);
-                GL11.glVertex2f(x - 50, y - 50);
-                GL11.glVertex2f(x + 50, y - 50);
-                GL11.glVertex2f(x + 50, y + 50);
-                GL11.glVertex2f(x - 50, y + 50);
-            GL11.glEnd();
-        GL11.glPopMatrix();
+    	 GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+         // Clear the screen and depth buffer
+      GL11.glMatrixMode(GL11.GL_PROJECTION);
+      GL11.glLoadIdentity();
+      GL11.glOrtho(0, 800, 600, 0, 800, -600);
+      GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+   
+         
+         GL11.glPushMatrix();
+         GL11.glLoadIdentity();
+         
+          GL11.glTranslatef(400,300,0.0f);
+         GL11.glRotatef(angle, 0.0f, 0.5f, 0.0f);
+         GL11.glRotatef(50, 0.0f, 0.0f, 1.0f);
+         GL11.glScalef(100,100,100);
+         
+         GL11.glBegin(GL11.GL_QUAD_STRIP);
+          {
+          //this Makes a box that has no top or bottom. 
+          //Front - Orange
+          //Right - White
+          //Back - Blue
+          //Left - Teal
+          
+          GL11.glColor4f(1.0f,0.5f,0.0f,1.0f); 
+          GL11.glVertex3f(0.0f,1.0f,0.0f); //Front Bottom Left
+          GL11.glVertex3f(0.0f,0.0f,0.0f); //Front Top Left
+          GL11.glVertex3f(1.0f,1.0f,0.0f); //Front Bottom Right
+          GL11.glVertex3f(1.0f,0.0f,0.0f); //Front Top Right
+          
+          GL11.glColor4f(1.0f,1.0f,1.0f,1.0f);
+          GL11.glVertex3f(1.0f,1.0f,1.0f); //Right Bottom Back
+          GL11.glVertex3f(1.0f,0.0f,1.0f); //Right Top Back
+
+
+          GL11.glColor4f(0.0f,0.0f,1.0f,1.0f);
+          GL11.glVertex3f(0.0f,1.0f,1.0f); //Back left Bottom
+          GL11.glVertex3f(0.0f,0.0f,1.0f); //Back Left Top
+          
+
+          GL11.glColor4f(0.0f,1.0f,1.0f,1.0f);
+          GL11.glVertex3f(0.0f,1.0f,0.0f); //Front Bottom Left
+          GL11.glVertex3f(0.0f,0.0f,0.0f); //Front Top Left
+          
+          }
+          GL11.glEnd(); 
+         GL11.glPopMatrix();
+         angle +=0.5f;
+         System.out.println(angle);
     }
   
     public static void main(String[] argv) {
