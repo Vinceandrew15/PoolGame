ab4900a22964d3ccf7b67a55f3ca15e1c4c9ece7
Danny Van Stemp <danwardvs@adsgames.net>
2016-02-23 08:58:25 -0500

    Pool table beginnings

--- src/FullscreenExample.java
+++ src/FullscreenExample.java
@@ -35,7 +35,7 @@ public class FullscreenExample {
 	
     public void start() {
         try {
-        Display.setDisplayMode(new DisplayMode(800,600));
+        Display.setDisplayMode(new DisplayMode(900,500));
         Display.create();
     } catch (LWJGLException e) {
         e.printStackTrace();
@@ -45,18 +45,14 @@ public class FullscreenExample {
     // init OpenGL
     GL11.glMatrixMode(GL11.GL_PROJECTION);
     GL11.glLoadIdentity();
-    GL11.glOrtho(0, 800, 0, 600, 1, -1);
+    GL11.glOrtho(0, 900, 0, 500, 1, -1);
     GL11.glMatrixMode(GL11.GL_MODELVIEW);
     
-    
-  
     while (!Display.isCloseRequested()) {
         // Clear the screen and depth buffer
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
          
-             
-        
-        drawRect(200,100,200,70,0.5f,0.5f,1.0f,0.0f);
+        drawRect(50,50,800,400,0f,1f,0.2f,0.0f);
         drawEllipse(400,200,50,300,1f,1f,0f,0.0f);
         
         Display.update();
